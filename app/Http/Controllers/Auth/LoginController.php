285261f816e49d36df6261cbb2627c7ff3e140a4
Marvin Cuellar Salinas <marvin.cuellas@uab.edu.bo>
2022-03-21 20:48:34 -0400

    Diseño y validacion de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -12,17 +12,6 @@ use App\User;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
     /**
@@ -32,13 +21,8 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+
+    public function __construct(){
         $this->middleware('guest')->except('logout');
     }
 
@@ -55,24 +39,20 @@ class LoginController extends Controller
         ]);
 
         if (Auth::attempt($cred)) {
-            $active = auth()->user()->active;
-            if ( $active == 1 ) {
+            if ( auth()->user()->active == 1 ) {
                 if (request()->remember) {
                     $lifetime = time() + 60 * 60 * 24 * 365;
                     Cookie::queue(Cookie::make('login1', request()->username, $lifetime));
                     Cookie::queue(Cookie::make('login2', request()->password, $lifetime));
-                }
-                else{
+                }else{
                     if(Cookie::has('login1') && Cookie::has('login1')){
                         Cookie::queue(Cookie::forget('login1'));
                         Cookie::queue(Cookie::forget('login2'));
                     }
                 }
                 return redirect()->action('HomeController@index');
-            } else {
+            } else
                 Auth::logout();
-                return back()->withErrors(['username' => 'Error de usuario', 'password' => 'Error de contraseña']);
-            }
         }
         return back()->withErrors(['username' => 'Error de usuario', 'password' => 'Error de contraseña']);
     }
