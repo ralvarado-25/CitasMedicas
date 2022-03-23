e1f078b1402d49c773449544aa2d4846914bacba
Benjamin Cacasaca Avila <benjamin.cacasaca@gmail.com>
2022-03-23 00:08:36 -0400

    Generar vista de listado de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,8 +12,19 @@ use Mail;
 class UserController extends Controller
 {
     public function index (Request $request){
+
+        $userlog = User::where('id',userId())->get();
+        $usersa = User::where('active',1)->where('id','!=',userId())->orderBy('ap_paterno','asc')->get();
+        $usersi = User::where('active','!=',2)->where('active','!=',1)->orderBy('ap_paterno','asc')->get();
+        $usersa = $userlog->merge($usersa);
+        $usersa = $usersa->merge($usersi);
+
+        $usersactive = User::where('active',1)->count();
+        $usersinactive = User::where('active',0)->count();
+        $usersdelete = User::where('active',2)->count();
+
         $filtro = 'ejemplo de variable';
         Session::put('item', '2.');
-        return view("adminTemplate.users.index", compact('filtro'));
+        return view("adminTemplate.users.index", compact('usersactive','usersinactive','usersdelete','usersa','usersi'));
     }
 }
