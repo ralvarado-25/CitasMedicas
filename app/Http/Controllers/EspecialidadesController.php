8a906bd546966178cc2a2f0a7fbbde22fcbd649d
Marvin Cuellar Salinas <marvin.cuellar@uab.edu.bo>
2022-03-26 23:53:36 -0400

    Funcion para crear especialidad odontologica

--- app/Http/Controllers/EspecialidadesController.php
+++ app/Http/Controllers/EspecialidadesController.php
@@ -24,6 +24,63 @@ class EspecialidadesController extends Controller
     }
 
     /**
+     * FUncion para guardar especialidad
+     */
+    public function store(Request $request){
+        $messages = [
+            'fileEspecialidades.mimes' => 'El archivo debe estar en algunos de los siguientes formatos:<br><p class="text-center"><b>gif, jpg, jpeg, png</b></p>',
+            'fileEspecialidades.max' => "El archivo a subir es muy grande. El tamaño máximo admitido es de 5 MB (5192 KB).",
+        ];
+        $validateArray = [
+            'nombre' => 'required',
+            'duracion' => 'required|numeric',
+            'descripcion' => 'required|min:2|max:255',
+            'fileEspecialidades' => 'bail|nullable|mimes:gif,jpg,jpeg,png|max:5192',
+        ];
+        $request->validate($validateArray, $messages);
+
+        $reg_maximo = Especialidades::select('cod')->where('cod', 'LIKE', "%ES%")->max('cod');
+        $cod = generateCode($reg_maximo,'ES000001','ES',2,6);
+        DB::beginTransaction();
+        try {
+            $especialidad = new Especialidades();
+            $especialidad->user_id = userId();
+            $especialidad->cod = $cod;
+            $especialidad->nombre = $request->nombre;
+            $especialidad->descripcion = $request->descripcion;
+            $especialidad->duracion = $request->duracion;
+            $especialidad->activo = "1";
+            if ( $request->hasFile('fileEspecialidades') ){
+                $archivo = $request->file('fileEspecialidades');
+                $nombreConExtension = $archivo->getClientOriginalName();
+                $nombreConExtension = delete_charspecial($nombreConExtension);
+                $especialidad->imagen = $cod . '_' . strtolower($nombreConExtension);
+                $archivo->storeAs("public/especialidad/", $especialidad->imagen);
+                $size = getimagesize($archivo);
+                if($size[0]<=1024 && $size[1]<=1024){
+                    InterventionImage::make($archivo)->resize(function ($constraint){
+                        $constraint->aspectRatio();
+                    })->save(storage_path().'/app/public/especialidad/'.$especialidad->imagen, 90);
+                }else{
+                    InterventionImage::make($archivo)->resize(1024,1024, function ($constraint){
+                        $constraint->aspectRatio();
+                    })->save(storage_path().'/app/public/especialidad/'.$especialidad->imagen, 80);
+                }
+            }
+            $especialidad->save();
+            toastr()->success('Registrado con éxito.','Especialidad '.$especialidad->nombre, ['positionClass' => 'toast-bottom-right']);
+            DB::commit();
+            return  \Response::json(['success' => '1']);
+        }catch (\Exception $e) {
+            DB::rollback();
+            return $e->getMessage();
+        }
+
+        toastr()->success('Registrado con éxito.','Ejemplo ', ['positionClass' => 'toast-bottom-right']);
+        return back();
+    }
+
+    /**
      * FUncion para actualizar registro
      */
     public function update(Request $request, $id){
---
+++ resources/views/adminTemplate/especialidades/modalCreate.blade.php
@@ -0,0 +1,118 @@
+@push('extracss')
+    <style>
+        .input-icon .form-control{
+            height: 36px;
+        }
+        .file-drop-zone-title {
+            padding: 0px !important;
+        }
+        .file-preview-frame{
+            height: 150px;
+        }
+        .kv-file-content, .file-preview-other{
+            height: 50px !important;
+        }
+        .file-other-icon{
+            font-size: 3em !important
+        }
+        .krajee-default.file-preview-frame {
+            left: 33%;
+        }
+        @media  (max-width: 991px){
+            .krajee-default.file-preview-frame {
+                left: 20%;
+            }
+        }
+        @media  (max-width: 767px){
+            .krajee-default.file-preview-frame {
+                left: 0%;
+            }
+        }
+        .file-caption-main, .kv-error-close{
+            display: none !important;
+        }
+    </style>
+@endpush
+<div class="modal modalPrimary fade modal-slide-in-right" aria-hidden="true" role="dialog"  id="modalCrearEspecialidad" data-backdrop="static">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><i class="fa fa-plus"></i> Nueva área de trabajo</h5>
+                <button type="button" class="btn-close text-primary" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                {!! Form::open( array('route' =>'especialidades.store','method'=>'POST','autocomplete'=>'off','files'=>'true','id'=>'formCreateEspecialidades', 'onsubmit'=>'btnSubmit.disabled = true; return true;'))!!}
+                <div class="row">
+                    {!! datosRegistro('create') !!}
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="form-group">
+                            <label id="nombre--label">* Nombre de especialidad</label> <br>
+                            <input class="form-control" name="nombre" type="text" placeholder="Nombre de nueva área">
+                            <span id="nombre-error" class="text-red"></span>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="form-group">
+                            <label id="duracion--label">* Duración aproximada (en horas)</label> <br>
+                            <input class="form-control" name="duracion" type="text" placeholder="Duración aproximada del tratamiento">
+                            <span id="duracion-error" class="text-red"></span>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="form-group">
+                            <label id="descripcion--label">* Descripción</label> <br>
+                            <textarea name="descripcion"  rows="3" class="form-control" style="width:100%;resize:none"></textarea>
+                            <span id="descripcion-error" class="text-red"></span>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 archivodiv" id="fileEspecialidades--label">
+                        <b>Adjuntar imagen (.gif, .jpg, .jpeg, .png)</b><br>
+                        <div id="fileEspecialidades_fg" class="form-group" style="margin:0; padding:0" >
+                            <input type="file" class="input-sm" id="fileEspecialidades" name="fileEspecialidades" data-max-size="5192" data-browse-on-zone-click="true" accept=".gif, .jpg, .jpeg, .png">
+                            <span id="fileEspecialidades-error" class="text-red"></span>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-2">
+                        <button type="button" class="btn btn-ghost-secondary pull-left" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary pull-right" name="btnSubmit">Registrar</button>
+                    </div>
+                </div>
+                {{Form::Close()}}
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@push('scripts')
+    <script>
+        $("#fileEspecialidades").fileinput({
+            showUpload: false,
+            allowedFileExtensions: ["gif","jpg","jpeg","png"],
+            maxFileSize: 5192,
+            // Máximo tamaño a previsualizar
+            maxFilePreviewSize: 5192,
+            previewClass: "bg-fileinput",
+            preferIconicPreview: true,
+            previewFileIconSettings: {
+                'docx': '<i class="fas fa-file-word text-primary"></i>',
+                'xlsx': '<i class="fas fa-file-excel text-success"></i>',
+                'pptx': '<i class="fas fa-file-powerpoint text-danger"></i>',
+                'pdf': '<i class="fas fa-file-pdf text-danger"></i>',
+                'zip': '<i class="fas fa-file-archive text-muted"></i>',
+            },
+            "fileActionSettings":{ "showZoom":true }
+        });
+        $('#fileEspecialidades_fg .file-caption').click(function(){
+            $('#fileEspecialidades').trigger('click');
+        });
+    </script>
+    <script>
+        var campos = ['nombre','duracion','descripcion','fileEspecialidades'];
+        ValidateAjax("formCreateEspecialidades",campos,"btnSubmit","{{route('especialidades.store')}}","POST","/especialidades");
+    </script>
+@endpush
